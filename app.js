1186c5d0db89840de92c8a8650a346e8a7f6490f
Dyuthi11 <nityaa55@gmail.com>
2020-10-15 00:41:39 +0530

    Add few more project files and make changes

--- app.js
+++ app.js
@@ -4,11 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 var _ = require("lodash");
-
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-let posts = [];
+let mysql = require('mysql');
 
 const app = express();
 
@@ -19,15 +15,36 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
+//Database Connection
+let connection = mysql.createConnection({
+  host: 'remotemysql.com',
+  user: 'NP2EPmxQOf',
+  password: 'oAoMSzPzpu',
+  database: 'NP2EPmxQOf'
+});
+
+connection.connect(function (err) {
+  if (err) {
+      return console.error('error: ' + err.message);
+  }
+
+  console.log('Connected to the MySQL server.');
+});
+
+
 
 // Home Route/Root Route
 
 app.get("/", function (req, res) {
   res.render("index");
-  // , {
-  //   homePageContent: homeStartingContent,
-  //   posts: posts
-  // });
+});
+
+// Home Route
+
+app.get("/home", function (req, res) {
+  res.render("home", {
+    status: "not ok"
+  });
 });
 
 
@@ -47,21 +64,6 @@ app.get("/contact", function (req, res) {
   });
 });
 
-
-// Compose Route
-app.get("/compose", function (req, res) {
-  res.render("compose");
-});
-
-app.post("/compose", function(req, res) {
-  var post = {
-    body: req.body.postBody,
-    title: req.body.postTitle
-  };
-  posts.push(post);
-  res.redirect("/");
-});
-
 app.get("/post/:postName", function(req, res) {
   let reqName = _.lowerCase(req.params.postName);
   
@@ -78,6 +80,28 @@ app.get("/post/:postName", function(req, res) {
     }
   });
 });
+
+app.post("/", (req, res) => {
+  const name = req.body.userName;
+  const pw = req.body.pw;
+  if (name === "admin" && pw === "root") {
+      res.redirect("home", { status: "ok" });
+  }
+  res.render("home", { status: "not ok" });
+});
+
+app.get("/employee/index", (req, res) => {
+  res.render("employee/display");
+});
+
+app.post("/employee", (req, res) => {
+  const num = req.body.no;
+  connection.query("select * from Employee where no=" + connection.escape(num), function (error, result, fields) {
+      if (error) throw error;
+      console.log(result[0].name);
+      res.render("index", { name: result[0].name, email: result[0].no, body: "dbc" })
+  });
+})
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
